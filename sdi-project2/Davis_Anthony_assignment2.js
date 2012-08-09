2f2f956737268f5d585325a81142a359cbf9ec81
Anthony Davis <anthonydavis@fullsail.edu>
2012-08-09 12:55:08 -0600

    Declared initial variables and created procedure function

--- sdi-project2/Davis_Anthony_assignment2.js
+++ sdi-project2/Davis_Anthony_assignment2.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+var $gym = "Golds";
+var hrsOfWorkout = "2";
+var $offDay = false;
+var $onDay = true;
+var $bodyPart = ["chest", "back", "shoulders", "legs", "arms"];
+
+// workOutToday($dayOff);
+
+intro($gym);
+
+function intro($gym)
+{
+    if(!$gym)
+    {
+        console.log("Brandon Bambino forgot the name of his gym, so he can't go workout today");
+    } else {
+        console.log("Brandon Bambino needed to go to " + $gym + " gym, but he couldn't decide which bodypart to workout.")
+    };
+
+};
+
