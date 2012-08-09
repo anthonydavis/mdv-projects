48981b668a62d9ac23b2fb3676d3cec9a6c51d72
Anthony Davis <anthonydavis@fullsail.edu>
2012-08-09 13:30:02 -0600

    Created Boolean Function

--- sdi-project2/Davis_Anthony_assignment2.js
+++ sdi-project2/Davis_Anthony_assignment2.js
@@ -1,12 +1,14 @@
 var $gym = "Golds";
 var hrsOfWorkout = "2";
-var $offDay = false;
-var $onDay = true;
+var $offDay = ["sunday"];
+var $onDay = ["monday", "tuesday", "wednesday", "thursday", "friday"];
 var $bodyPart = ["chest", "back", "shoulders", "legs", "arms"];
 
 // workOutToday($dayOff);
 
 intro($gym);
+
+workOut($offDay, $onDay);
 
 function intro($gym)
 {
@@ -16,6 +18,18 @@ function intro($gym)
     } else {
         console.log("Brandon Bambino needed to go to " + $gym + " gym, but he couldn't decide which bodypart to workout.")
     };
+
+};
+
+function workOut( $offDay, $onDay )
+{
+    if( $onDay[0] == "monday" || $onDay[1] == "tuesday" && $onDay[2] == "wednesday" && $onDay[3] == "thursday" || !$onDay[0] == "sunday" )
+    {
+        console.log( "He first decided to check his workout log and make sure today was a day off from the gym or not, and it looks like he needs to go" );
+    }else
+     {
+        console.log( "He first decided to check his workout log and make sure today was a day off from the gym or not, and it looks like he gets to rest for the day" );
+     };
 
 };
 
