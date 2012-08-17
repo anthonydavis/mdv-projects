a0da3df31b0200550c6641f78d96215b15b8f770
Anthony Davis <anthonydavis@fullsail.edu>
2012-08-16 19:50:49 -0600

    Created referral function

--- sdi-project3/Davis_Anthony_assignment3.js
+++ sdi-project3/Davis_Anthony_assignment3.js
@@ -5,6 +5,8 @@
 
 // Global Variables
 var intro = "Brandon Bambino finally made it to the gym!!! ";
+var friends = ["kevin", "troy", "lewis"];
+var response = false;
 
 // module pattern
 var profile = function(name){
@@ -34,6 +36,20 @@ var profile = function(name){
 
     };
 
+    // function to find response to personal training sign up - return boolean
+    var wantToSignUp = function(response){
+        if(!response){
+            return response;
+        };
+    };
+
+    var referral = function(friends){
+        // does he have friends to referr?
+        if(friends){
+            return friends;
+        }
+    };
+
     // mutator function
 
 
@@ -42,36 +58,68 @@ var profile = function(name){
         "name"                : name,
         "outputYearsTraining" : outputYearsTraining,
         "bodyParts"           : bodyParts,
-        "getFavoriteBodyPart" : getFavoriteBodyPart
+        "getFavoriteBodyPart" : getFavoriteBodyPart,
+        "wantToSignUp"        : wantToSignUp
     };
 
 };
 
-console.log(intro);
-console.log("To his surprise one of the trainers wants to give him a free session, but first he must create a profile for him.");
-console.log("After some lengthy questions, the trainer puts together a profile.");
+var referral = function(friends){
+    // does he have friends to referr?
+    if(friends){
+        return friends;
+    }
+};
+
+var printStory = function(intro, brandonProfile){
+    console.log(intro);
+    console.log("To his surprise one of the trainers wants to give him a free session, but first he must create a profile for him.");
+    console.log("After some lengthy questions, the trainer puts together a profile.");
+
+    var brandonProfile = profile("Brandon");
+    var yearsTraining = brandonProfile.outputYearsTraining();
+
+    console.log("From the information Brandon Bambino provided, here is his profile:");
+    console.log("   " + "Name: " + brandonProfile.name);
+    console.log("   " + "Years Training: " +  yearsTraining);
+    console.log("   " + "Favorite Bodypart: " + brandonProfile.getFavoriteBodyPart());
+    console.log("Brandon knew there would be a catch, after the profile was created he was pitched personal Training.");
+
+
+
+}
 
 var brandonProfile = profile("Brandon");
-var yearsTraining = brandonProfile.outputYearsTraining();
 
+
+printStory(intro);
+    
 console.log("From the information Brandon Bambino provided, here is his profile:");
 console.log("   " + "Name: " + brandonProfile.name);
 console.log("   " + "Years Training: " +  yearsTraining);
+console.log("   " + "Favorite Bodypart: " + brandonProfile.getFavoriteBodyPart());
+console.log("Brandon knew there would be a catch, after the profile was created he was pitched personal Training.");
 
-// function to get favorite bodypart
-var getFavoriteBodyPart = function(){
-
-
-    for(i = 0; i < brandonProfile.bodyParts.length; i++){
-        if(brandonProfile.bodyParts[i] === "arms"){
-            var favoriteBodyPart = brandonProfile.bodyParts[i];
-        };
-    };
-
-    return favoriteBodyPart;
-
+var hisResponse = brandonProfile.wantToSignUp(response);
+if(!hisResponse){
+    var answer = "No Thanks";
 };
 
-console.log("   " + "Favorite Bodypart: " + brandonProfile.getFavoriteBodyPart());
+console.log("After the trainer was done talking he asked Brandon if he wanted to sign up, his response was: " + answer);
+
+console.log("The Trainer asked Brandon if he wanted to list any friends as possible referrals, these are the names he listed:" );
+
+var referral = referral(friends);
+
+console.log("   " + friends[0] );
+console.log("   " + friends[1] );
+console.log("   " + friends[2] );
+
+
+
+
+
+
+
 
 
