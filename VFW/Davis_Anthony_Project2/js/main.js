4f38cd2633b3b70a154128be723f0f5bed9153d8
Anthony Davis <anthonydavis@fullsail.edu>
2012-09-08 16:32:43 -0600

    Completed array function

--- VFW/Davis_Anthony_Project2/js/main.js
+++ VFW/Davis_Anthony_Project2/js/main.js
@@ -17,8 +17,27 @@ window.addEventListener("DOMContentLoaded", function(){
         return theElement;
     }
 
+    // Create select field element and populate it with options
+    function makeCats(){
+        var formTag     = document.getElementsByTagName("form"), // formTag is an array of all the form tags
+            selectLi    = $("select"),
+            makeSelect  = document.createElement("select");
+            makeSelect.setAttribute("id", "category");
+
+        for(var i=0, j=musicCategory.length; i<j; i++){
+            var makeOption  = document.createElement("option");
+            var optText     = musicCategory[i];
+            makeOption.setAttribute("value", optText);
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        }
+        selectLi.appendChild(makeSelect);
+
+    }
+
     // Variable defaults
     var musicCategory = ["--Choose A style Category--", "80\'s", "Classical", "Country", "Metal", "Rap", "Rock"];
+    makeCats();
 
     // Set Link & Submit Click Events
     var displayLink = $("displayLink");
