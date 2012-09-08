2b690bdce4c9910e6e85321267e7716f7d9c75e7
Anthony Davis <anthonydavis@fullsail.edu>
2012-09-08 15:57:02 -0600

    Started main.js

--- VFW/Davis_Anthony_Project2/js/main.js
+++ VFW/Davis_Anthony_Project2/js/main.js
@@ -1,7 +1,37 @@
 /**
- * Created with JetBrains PhpStorm.
- * User: anthonydavis
- * Date: 9/7/12
- * Time: 6:13 PM
- * To change this template use File | Settings | File Templates.
- */
+ *
+ * Name: Anthony Davis
+ * Class: VFW
+ * Term: 1209
+ * Project: Web App Part 2
+ *
+ **/
+
+// Wait until Dom is ready
+window.addEventListener("DOMContentLoaded", function(){
+
+    // getElementByID function
+    function $(x){
+        var theElement = document.getElementById(x);
+
+        return theElement;
+    }
+
+    // Variable defaults
+    var musicCategory = ["--Choose A style Category--", "80\'s", "Classical", "Country", "Metal", "Rap", "Rock"];
+
+    // Set Link & Submit Click Events
+    var displayLink = $("displayLink");
+    displayLink.addEventListener("click", getData);
+
+    var clearLink = $("clearLink");
+    clearLink.addEventListener("click", clearData);
+
+    var submitButton = $("submitButton");
+    submitButton.addEventListener("click", storeData);
+
+
+
+
+});
+
